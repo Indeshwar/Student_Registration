8db9a7e4439ff219f4b02432132014fdeed74055
Indeshwar <indeshc@gmail.com>
2023-08-16 20:16:44 -0500

    SR-4 create User entity

---
+++ src/main/java/com/web/student_register/entity/User.java
@@ -0,0 +1,29 @@
+package com.web.student_register.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO )
+    private Long userId;
+    private String userName;
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable( name = "user_role",
+            joinColumns = @JoinColumn(name="user_id", referencedColumnName= "userId" ),
+            inverseJoinColumns = @JoinColumn(name= "role_id", referencedColumnName="roleId")
+    )
+    private Collection<Role> roles = new ArrayList<>();
+
+}

990c461fdd055fd7c7835483cf8abe8f0d45a0fe
Indeshwar <indeshc@gmail.com>
2023-09-13 15:44:36 -0500

    SR-9 Add the @ManyToMany relationship among Student and Teacher class

--- src/main/java/com/web/student_register/entity/User.java
+++ src/main/java/com/web/student_register/entity/User.java
@@ -21,6 +21,22 @@ public class User {
     private String password;
 
     @JsonManagedReference
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name="user_student",
+    joinColumns = @JoinColumn(name="user_id", referencedColumnName="userId" ),
+            inverseJoinColumns = @JoinColumn(name="student_id", referencedColumnName= "studentId")
+    )
+    private Collection<Student> students = new ArrayList<>();
+
+    @JsonManagedReference
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name="user_teacher",
+            joinColumns = @JoinColumn(name="user_id", referencedColumnName="userId" ),
+            inverseJoinColumns = @JoinColumn(name="teacher_id", referencedColumnName= "teacherId")
+    )
+    private Collection<Teacher> teachers = new ArrayList<>();
+
+    @JsonManagedReference
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable( name = "user_role",
             joinColumns = @JoinColumn(name="user_id", referencedColumnName= "userId" ),

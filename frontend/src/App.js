4ce503b9189efae6268de4d4ff8ee021a8f32180
Indeshwar <indeshc@gmail.com>
2023-10-10 01:45:01 -0500

    SR-16 Configure Router, Routes, Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import LogIn from "./Compoment/LogIn";
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Register from "./Compoment/Register";
 function App() {
   return (
-    <>
-     <LogIn/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LogIn/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+     
+    </Router>
   );
 }
 

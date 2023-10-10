3b1d13111d0d5273aa3a11d56ea5fba07321ff1a
Indeshwar <indeshc@gmail.com>
2023-10-10 01:42:02 -0500

    SR-16 Add link to Register page

--- frontend/src/Compoment/LogIn.js
+++ frontend/src/Compoment/LogIn.js
@@ -1,44 +1,57 @@
 import React from "react";
 import "./Login.css";
-
+import { Link } from "react-router-dom";
+import Footer from "./Footer";
 const LogIn = () => {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-12 col-sm-8 col-md-4 col-lg-4 mt-5">
-          <h2 className="h2x">This is my demo LogIn Page </h2>
-        </div>
-        <div className="col-12 col-sm-8 col-md-8 col-lg-3 mt-5 bg-primary-subtle rounded-2 pt-4 pd-3">
-          <form>
-            <h1 className="h1f">Student LogIn</h1>
-            <div className="m-4">
-              <input className="form-control" placeholder="UserName" />
-            </div>
-            <div className="m-4">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-              />
-            </div>
-            <div className="m-4">
-              <button type="submit" className="btn btn-primary">
-                Log In
-              </button>
-            </div>
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 col-sm-8 col-md-4 col-lg-4 mt-5">
+          </div>
+          <div className="col-12 col-sm-8 col-md-8 col-lg-3 mt-5">
+            <div className="main-content">
+              <form className="bg-primary-subtle rounded-2  p-4">
+                <h1 className="h1f">Student LogIn</h1>
+                <div className="m-4">
+                  <input className="form-control" placeholder="UserName" />
+                </div>
+                <div className="m-4">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="m-4">
+                  <button type="submit" className="btn btn-primary">
+                    Log In
+                  </button>
+                </div>
 
-            <h2 className="h2f">Forgot password</h2>
-          </form>
+                <h2 className="h2f">Forgot password</h2>
+              </form>
+            </div>
+          </div>
+        </div>
+        {/* row closing */}
+        <div className="row mt-2">
+          <div className="col-12 col-sm-8 col-md-4 col-lg-4"></div>
+
+          <div className="col-12 col-sm-8 col-md-8 col-lg-3">
+            <div className="main-content-2">
+              <div className="bg-primary-subtle rounded-2 p-4">
+                Don't Have an account?
+                <Link to="/register" className="h2f">
+                  Sign up
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      {/* row closing */}
-      <div className="row mt-2">
-        <div className="col-12 col-sm-8 col-md-4 col-lg-4"></div>
-        <div className="col-12 col-sm-8 col-md-8 col-lg-3 bg-primary-subtle rounded-2">
-          <h1 className="h2f">Don't Have an account? Singn up</h1>
-        </div>
-      </div>
-    </div> //container-fluid closing
+      <Footer />
+    </>
   );
 };
 
